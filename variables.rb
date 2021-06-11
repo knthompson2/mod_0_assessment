cf9f8b0a5a649cdd8d485f6ff95d8823d633dbcc
Kelsey Thompson <kelsthompson2@gmail.com>
2021-06-11 15:17:33 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,12 @@
+movie = "The Parent Trap"
+is_entertaining = true
+
+movie = "Cruella"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+
+favorite_foods = ["pizza", "nachos", "zucchini"]
+favorite_foods.delete("pizza")
+
+puts movie.length
