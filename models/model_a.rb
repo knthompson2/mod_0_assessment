7e50aafe4085c079a976582b5f039b8897a3901e
Kelsey Thompson <kelsthompson2@gmail.com>
2021-06-11 15:26:34 -0400

    Add dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  name: "Dudley",
+  color:  "green",
+  number_legs: 4,
+  weight: 4039,
+  herbivore: true,
+  can_fly: false,
+  hobbies: ["roaming around", "eating plants", "dino talk with friends"],
+  defining_characteristics: ["long neck","long tail", "scaly skin"]
+}
