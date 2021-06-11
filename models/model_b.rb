71adf169f6d1e65c0a9bf3f4aa3356425b48d9bc
Kelsey Thompson <kelsthompson2@gmail.com>
2021-06-11 15:33:10 -0400

    Add developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  name: "Kelsey",
+  program: "Backend",
+  age: 27,
+  years_experience: 0.1,
+  expert: false,
+  ready_to_learn: true,
+  areas_to_practice: ["Classes", "instances", "technical diction"],
+  related_skils: ["calendaring/time management", "pseudocoding", "Googling"]
+}
